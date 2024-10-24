9cab87936ef051de4be8233a0aa3da601bb712d7
Saumya Chandra <saumyassc3002@gmail.com>
2024-10-25 00:54:15 +0530

    Updated STATIC_ROOT

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,7 +119,8 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'

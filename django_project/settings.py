23b33043dcadeee5206689620346feed10049cd2
SaumyaChandra-git <saumyassc3002@gmail.com>
2025-02-15 17:49:49 +0530

    Add vercel file

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8z^otkn-p8c_c8zh)=e1dp^+pnqjleywjw8o(2knc%00yy*5l%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
